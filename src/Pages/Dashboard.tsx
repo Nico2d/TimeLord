1e909287e6702e644010a2cf88d7c5533e2c478f
Nico <nico30d@gmail.com>
2021-06-16 10:50:19 +0200

    feat: add Complementary

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Complementary } from "../Components/Complementary/Complementary";
 import { Main } from "../Components/Main/Main";
 import { NavigationSidebar } from "../Components/NavigationSidebar/NavigationSidebar";
 
@@ -7,6 +8,7 @@ export const Dashboard = () => {
     <Container>
       <NavigationSidebar userID={1} />
       <Main />
+      <Complementary />
     </Container>
   );
 };
